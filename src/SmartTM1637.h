132fdb5c0ecc734b0b033434b68258db6672be6f
fadhil-1911 <fadhil.hashim755@yahoo.com>
2025-07-09 11:07:52 +0800

    Update SmartTM1637.h

--- src/SmartTM1637.h
+++ src/SmartTM1637.h
@@ -1,16 +1,35 @@
 
-#ifndef AUTOTM1637DISPLAY_H
-#define AUTOTM1637DISPLAY_H
+#ifndef SMARTTM1637DISPLAY_H
+#define SMARTTM1637DISPLAY_H
 
 #include <Arduino.h>
 
-class AutoTM1637Display {
+class SmartTM1637 {
 public:
-  AutoTM1637Display(uint8_t clk, uint8_t dio);
+  SmartTM1637(uint8_t clk, uint8_t dio);
   void begin(uint8_t brightness = 7);
   void print(const char* text);
-  void printNumber(int num, bool leadingZero = false); // fungsi tambahan 1
-  void print(int val, const char* suffix); // fungsi tambahan 2
+
+  // fungsi tambahan 1
+  void printNumber(int num, bool leadingZero = false);
+  // fungsi tambahan 2
+  void print(int val, const char* suffix);
+
+  // fungsi tambahan 3 01JULY25
+  void print(float val, const char* suffix, bool showDecimal = false);                    // Beginner
+  void print(float val, const char* suffix, bool dot1, bool dot2, bool dot3, bool dot4);  // Pro
+  void printWithDots(const char* text, uint8_t dotMask = 0);                              // Utiliti dalaman
+
+  // fungsi tambahan 4 01JULY25
+  void print(float val, bool dot1, bool dot2, bool dot3, bool dot4);
+
+  // fungsi tambahan 5 01JULY25
+  void print(const char* text, bool dot1, bool dot2, bool dot3, bool dot4);  // Beginner-friendly overload
+
+  void printTime(uint8_t hour, uint8_t minute, bool showColon = true);
+
+  // fungsi tambahan 6 02JULY25
+  //void printWithDots(const char* text, uint8_t dotMask = 0);
   void clear();
 
 private:
@@ -25,3 +44,9 @@ private:
 };
 
 #endif
+
+
+
+
+
+
