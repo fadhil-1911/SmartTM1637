35ed0e52ecd98fc4a175685b0ee98a0f2bc7a76f
fadhil-1911 <fadhil.hashim755@yahoo.com>
2025-08-01 23:58:59 +0800

    Update SmartTM1637.cpp

--- src/SmartTM1637.cpp
+++ src/SmartTM1637.cpp
@@ -4,8 +4,8 @@
 *                 with extended features such as decimal point,
 *                 text, time display, and brightness control.
 *
-*  Version      : 1.0
-*  Date         : 9 July 2025
+*  Version      : 1.0.1
+*  Date         : 01 August 2025
 *  Platform     : Arduino (Uno/Nano/Mega, etc.)
 *  Author       : [fadhil /  SmartTM1637]
 *
